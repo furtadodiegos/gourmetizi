fcf6daae467c9da47ab4978c4bddb75ba982cdb1
Diego Furtado <furtado.diegos@gmail.com>
2023-01-13 14:24:16 -0300

    feat(chore): assets
    - new assets
    - temp changes on index to check the assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import Lottie from 'lottie-react-native';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import type { FC } from 'react';
 
-// LogBox.ignoreLogs(['Remote debugger']);
+import loading from './assets/animation.json';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,11 +16,51 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
+const App: FC = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [dataIsReady, setDataIsReady] = useState(false);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) await hideAsync();
+  }, [appIsReady]);
+
+  useEffect(() => {
+    (async () => {
+      preventAutoHideAsync();
+
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(setAppIsReady(true));
+        }, 1000);
+      });
+    })();
+  }, []);
+
+  useEffect(() => {
+    async function asyncData() {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(setDataIsReady(true));
+        }, 5000);
+      });
+    }
+
+    if (appIsReady) asyncData();
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your appx!!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
+
+      {dataIsReady ? (
+        <Text>Open up App.js to start working on your appx!!</Text>
+      ) : (
+        <Lottie source={loading} autoPlay loop speed={3} />
+      )}
     </View>
   );
-}
+};
+
+export default App;
